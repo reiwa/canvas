7f1ee54c2b53ac33160b9f23dc7f0cf14b16cb08
uufish <uufish@sankaku.io>
2017-07-31 01:41:55 +0900

    refactor

--- src/canvas/index.js
+++ src/canvas/index.js
@@ -2,17 +2,8 @@ window.addEventListener('load', main)
 
 function main () {
   const canvas = document.querySelector('.canvas')
-  setCanvasPosition(canvas)
   setCanvasSize(canvas)
-  window.addEventListener('resize', function () { setCanvasSize(canvas) })
-}
-
-function setCanvasPosition (canvas) {
-  canvas.style.position = 'fixed'
-  canvas.style.top = 0
-  canvas.style.left = 0
-  canvas.style.width = '100%'
-  canvas.style.zIndex = -1
+  window.addEventListener('resize', () => { setCanvasSize(canvas) })
 }
 
 function setCanvasSize (canvas) {
